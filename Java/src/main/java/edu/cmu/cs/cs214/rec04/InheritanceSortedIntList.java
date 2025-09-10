653066a4d4bbed6066a956acc8daa4979b2e8345
yaqing-zhou <yaqingz@andrew.cmu.edu>
2025-09-10 12:52:08 -0400

    feat(InheritanceSortedIntList.java): finished implementation with inheritance

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -11,7 +11,31 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
+
+    public int getTotalAdded(){
+        return totalAdded;
+    }
+
+    @Override
+    public boolean add(int num){
+        totalAdded++;
+        return super.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+
+        boolean success = false;
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            success |= this.add(list.get(i));
+        }
+
+        return success;
+    }
 
 }
