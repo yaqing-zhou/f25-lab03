39b659eb0ba702129f7ba80473e1519f5e92de8d
yaqing-zhou <yaqingz@andrew.cmu.edu>
2025-09-10 12:52:33 -0400

    feat(DelegationSortedIntList.java): finished implementation with delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,52 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private final SortedIntList delegate = new SortedIntList();
 
+    private int totalAdded = 0;
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    @Override
+    public boolean add(int num){
+        totalAdded++;
+        return delegate.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+
+        boolean success = false;
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            success |= this.add(list.get(i));
+        }
+
+        return success;
+    }
+
+    @Override
+    public int get(int index){
+        return delegate.get(index);
+    }
+
+    @Override
+    public boolean remove(int num){
+        return delegate.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list){
+        return delegate.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
 }
