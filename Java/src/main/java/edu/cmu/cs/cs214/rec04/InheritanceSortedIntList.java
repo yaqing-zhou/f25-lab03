b64d47ba75eb77242f315d8f834dfddd5377da16
yaqing-zhou <yaqingz@andrew.cmu.edu>
2025-09-10 21:29:20 -0400

    Revert "fix(InheritanceSortedIntList.java): reuse super class's addAll while avoiding double counting"
    This reverts commit 02e04ccf5cda7f7ea6c4f645fbd8b57416859b23.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -15,30 +15,27 @@ public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
     private int totalAdded = 0;
 
-    private boolean skipCount = false; // whether count should be skipped
-
     public int getTotalAdded(){
         return totalAdded;
     }
 
     @Override
     public boolean add(int num){
-        if (!skipCount) {
-            totalAdded++;
-        }
+        totalAdded++;
         return super.add(num);
     }
 
     @Override
     public boolean addAll(IntegerList list) {
-        totalAdded += list.size();
-        skipCount = true; // avoid double counting when super class calls addAll
 
-        try {
-            return super.addAll(list);
-        } finally {
-            skipCount = false; // restore state
+        boolean success = false;
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            success |= this.add(list.get(i));
         }
+
+        return success;
     }
 
 }
